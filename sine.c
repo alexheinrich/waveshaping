e54de6054bdbeb2c44a5a45de4432708e13db4ca
alexheinrich <alex.heinrich@icloud.com>
2018-06-28 18:47:45 +0200

    Add basic Phase modulation and LFO.

--- sine.c
+++ sine.c
@@ -10,7 +10,7 @@
 #define TWOPI PI * 2
 
 #define BUF_SZ 16
-#define NUM_OSC 16
+#define NUM_OSC 1
 
 typedef enum {
 	PRESSED,
@@ -22,6 +22,7 @@ typedef enum {
 typedef struct {
 	double twopiovrsr;
 	double curfreq;
+	double stagedfreq;
 	double curphase;
 	double incr;
 } oscil_t;
@@ -35,9 +36,11 @@ typedef struct {
 	double max_volume;
 } env_t;
 
-oscil_t *oscillators[NUM_OSC];
-float staged_frequencies[NUM_OSC]; // TODO: merge into oscil_t
 int32_t current_oscillator = 0;
+
+oscil_t *oscillators[NUM_OSC];
+oscil_t *modulators[NUM_OSC];
+oscil_t *lfos[NUM_OSC];
 
 env_t envelopes[NUM_OSC];
 
@@ -63,34 +66,11 @@ oscil_t *oscil(void)
 	return osc;
 }
 
-double sine_tick(oscil_t* p_osc, double freq)
+void oscil_phase_incr(oscil_t* p_osc)
 {
-	double tick;
-
-	tick = (double) sin(p_osc->curphase);
-
-	int32_t int_tick = (int32_t) (tick * 115.0 + 115.0);
-	tick = 0;
-	//printf("int tick %d\n", int_tick);
-	//printf("tick %f\n", tick);
-
-	for (uint32_t i = 1; i < 5; i++) {
-		double knm = ((double) knmtab[i][int_tick] + 1022.0) / 2048.0;
-		tick = tick + knm - tick * knm;
-	}
-
-
-	//printf("tick %f\n", tick);
-
-	//tick = tick/32.0;
-
-	if (tick > 1.0 || tick < -1.0) {
-		printf("Amplitude has exceed 1 %f\n", tick);
-	}
-
-	if (p_osc->curfreq != freq) {
-		p_osc->curfreq = freq;
-		p_osc->incr = p_osc->twopiovrsr * freq;
+	if (p_osc->curfreq != p_osc->stagedfreq) {
+		p_osc->curfreq = p_osc->stagedfreq;
+		p_osc->incr = p_osc->twopiovrsr * p_osc->stagedfreq;
 	}
 
 	p_osc->curphase += p_osc->incr;
@@ -102,8 +82,37 @@ double sine_tick(oscil_t* p_osc, double freq)
 	if (p_osc->curphase < 0.0) {
 		p_osc->curphase += TWOPI;
 	}
+}
 
-	return sin(p_osc->curphase);
+double calc_tick(oscil_t* p_osc, oscil_t* p_mod, oscil_t* p_lfo)
+{
+//	double tick;
+
+//	tick = (double) sin(p_osc->curphase);
+//
+//	int32_t int_tick = (int32_t) (tick * 115.0 + 115.0);
+//	tick = 0;
+//	//printf("int tick %d\n", int_tick);
+//	//printf("tick %f\n", tick);
+//
+//	for (uint32_t i = 1; i < 5; i++) {
+//		double knm = ((double) knmtab[i][int_tick] + 1022.0) / 2048.0;
+//		tick = tick + knm - tick * knm;
+//	}
+
+
+	//printf("tick %f\n", tick);
+
+	//tick = tick/32.0;
+//
+//	if (tick > 1.0 || tick < -1.0) {
+//		printf("Amplitude has exceed 1 %f\n", tick);
+//	}
+	oscil_phase_incr(p_osc);
+	oscil_phase_incr(p_mod);
+	oscil_phase_incr(p_lfo);
+
+	return cos(p_osc->curphase + (4 + 2 * sin(p_lfo->curphase)) * sin(p_mod->curphase));
 }
 
 static double calc_volume(int32_t osc_number)
@@ -120,7 +129,8 @@ static double calc_volume(int32_t osc_number)
 		if (envelopes[osc_number].note_status == RESETTING) {
 			envelopes[osc_number].note_status = PRESSED;
 			envelopes[osc_number].increment = envelopes[current_oscillator].max_volume / envelopes[current_oscillator].attack;
-		} else {
+		}
+		else {
 			envelopes[osc_number].note_status = OFF;
 			envelopes[osc_number].increment = 0.0;
 		}
@@ -150,7 +160,7 @@ static void audio_callback(void *udata, uint8_t *stream, int32_t len)
     		float stream_buffer = 0;
 
     		for (int32_t i = 0; i < NUM_OSC; i++) {
-    			double envelope_point = calc_volume(i) * sine_tick(oscillators[i], staged_frequencies[i]);
+    			double envelope_point = calc_volume(i) * calc_tick(oscillators[i], modulators[i], lfos[i]);
     			stream_buffer = envelope_point + stream_buffer - envelope_point * stream_buffer;
     		}
 
@@ -179,7 +189,7 @@ static void midi_callback(double timeStamp, const unsigned char* message, size_t
 
 	if ((uint8_t) message[2] == 0) {
 		for (int32_t i = 0; i < NUM_OSC; i++) {
-			if (staged_frequencies[i] == note_frequency) {
+			if (oscillators[i]->stagedfreq == note_frequency) {
 				envelopes[i].note_status = RELEASED;
 				envelopes[i].increment = envelopes[i].current_value / envelopes[i].release * -1.0;
 			}
@@ -189,14 +199,14 @@ static void midi_callback(double timeStamp, const unsigned char* message, size_t
 	}
 
 	for (int32_t i = 0; i < NUM_OSC; i++) {
-		if (staged_frequencies[i] == note_frequency) {
+		if (oscillators[i]->stagedfreq == note_frequency) {
 			envelopes[i].note_status = RESETTING;
 			envelopes[i].increment = envelopes[i].current_value / 200.0 * -1.0;
 			return;
 		}
 	}
 
-	staged_frequencies[current_oscillator] = note_frequency;
+	oscillators[current_oscillator]->stagedfreq = note_frequency;
 	if (envelopes[current_oscillator].note_status != OFF) {
 		envelopes[current_oscillator].note_status = RESETTING;
 		envelopes[current_oscillator].increment = 0.0;
@@ -230,12 +240,12 @@ void mid_init(void)
 
 	rtmidi_open_port(midiin, 0, rtmidi_get_port_name(midiin, 0));
 	if(!midiin->ok) {
-		printf("printfed to open Midi port");
+		printf("failed to open MIDI port");
 	}
 
 	rtmidi_in_set_callback(midiin, midi_callback, midiin->data);
 	if(!midiin->ok) {
-		printf("printfed to set Midi Callback");
+		printf("failed to set MIDI Callback");
 	}
 }
 
@@ -262,9 +272,18 @@ static void init(void)
     for(int32_t i = 0; i < NUM_OSC; i++) {
     		oscillators[i] = oscil();
     		oscil_init(oscillators[i], SAMPLING_RATE);
-    		staged_frequencies[i] = 440.0;
+    		oscillators[i]->stagedfreq = 440.0;
+
+    		modulators[i] = oscil();
+    		oscil_init(modulators[i], SAMPLING_RATE);
+    		modulators[i]->stagedfreq = 200.0;
+
+    		lfos[i] = oscil();
+    		oscil_init(lfos[i], SAMPLING_RATE);
+    		lfos[i]->stagedfreq = 0.1;
+
     		envelopes[i].note_status = OFF;
-    		envelopes[i].current_value = 0.0;
+    		envelopes[i].current_value = 0.6;
     		envelopes[i].increment = 0.0;
     		envelopes[i].attack = 5000.0;
     		envelopes[i].release = 40000.0;
@@ -296,6 +315,8 @@ static void quit(void)
 
     for(int32_t i = 0; i < NUM_OSC; i++) {
     		free(oscillators[i]);
+    		free(modulators[i]);
+    		free(lfos[i]);
     }
 
     mid_quit();
